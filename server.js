e1eef235501d57c763c30a700a6b5246517fb791
Yongsheng <ygong56@uwo.ca>
2024-06-29 13:02:35 -0400

    update

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const app = express();
 app.use(express.json());
 dotenv.config();
 const port = process.env.PORT||8080;
-console.log(process.env.FRONT_END);
 app.use(cors({ origin: process.env.FRONT_END }));
 
 app.get('/', (req, res) => {
